fb23379cc9eb09c1c8e979eb5a320ab337a6fad8
Arye Eidelman <aryedov.e@gmail.com>
2020-06-18 01:38:03 -0400

    Added basic info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Arye Eidelman</h1>
       </header>
+
+      <article class="auto-grid">
+
+        <section>
+          <h2>Profile links</h2>
+          <nav>
+            <ul>
+              <li><a href="https://github.com/arye-dov-eidelman">Github</a></li>
+              <li><a href="https://twitter.com/arye_eidelman">Twitter</a></li>
+              <li><a href="https://stackoverflow.com/users/3458162/arye-eidelman">Stack Overflow</a></li>
+              <li><a href="https://www.linkedin.com/in/arye-eidelman/">Linkedin</a></li>
+              <li><a href="mailto:aryedoveidelman@gmail.com">aryedoveidelman@gmail.com</a></li>
+            </ul>
+          </nav>
+        </section>
+
+        <section>
+          <h2>Projects</h2>
+          <nav>
+            <ul>
+              <li><a href="https://mafteiach.app">Mafteiach</a></li>
+              <li><a href="https://www.linktotel.com/">Link to Tel</a></li>
+              <li><a href="http://offlinechess.app/">chess</a></li>
+            </ul>
+          </nav>
+        </section>
+
+        <section>
+          <h2>Languages, Frameworks, & Tools</h2>
+          <ul>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>CSS</li>
+            <li>Ruby</li>
+            <li>Rails</li>
+            <li>SQL</li>
+            <li>Git</li>
+            <li>HTML</li>
+            <li>TypeScript</li>
+            <li>Sass</li>
+          </ul>
+        </section>
+
+      </article>
     </div>
   );
 }
